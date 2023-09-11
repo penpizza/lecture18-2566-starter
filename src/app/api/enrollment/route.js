8128263315b25f12ce1cad8c54b4fab45a2a2fa8
penpizza <penpicha.thongkham@gmail.com>
2023-09-11 15:09:30 +0700

    ok

--- src/app/api/enrollment/route.js
+++ src/app/api/enrollment/route.js
@@ -1,14 +1,30 @@
 import { DB } from "@/app/libs/DB";
+import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
+import { headers } from "next/headers";
 
 export const GET = async (request) => {
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+  const rawAuthHeader = headers().get("authorization");
+  //console.log(rawAuthHeader);
+
+  const token = rawAuthHeader.split(" ")[1];
+  //spit -> array of string ['Bearer' , '......token']
+
+  let studentId = null;
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    //console.log(payload);
+    studentId = payload.studentId;
+    //console.log(studentId);
+  } catch {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+  }
 
   const courseNoList = [];
   for (const enroll of DB.enrollments) {
@@ -23,6 +39,27 @@ export const GET = async (request) => {
 };
 
 export const POST = async (request) => {
+  const rawAuthHeader = headers().get("authorization");
+  //console.log(rawAuthHeader);
+
+  const token = rawAuthHeader.split(" ")[1];
+  //spit -> array of string ['Bearer' , '......token']
+
+  let studentId = null;
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    //console.log(payload);
+    studentId = payload.studentId;
+    //console.log(studentId);
+  } catch {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
+  }
   //read body request
   const body = await request.json();
   const { courseNo } = body;
